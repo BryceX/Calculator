4a7bc2a3791cffbacba6ed69bb0a8a5e165aa0ba
BryceX <btau20102@yahoo.com>
2015-04-08 13:21:09 -0700

    crappy calculator

---
+++ Learning Csharp/MyClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Csharp
+{
+    public class MyClass
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2 ;
+        }
+        public int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multi(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public float Div(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ Learning Csharp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Learning_Csharp
+{
+    public class myClass
+    {
+        public struct MyStruct
+        {
+            public static int x = 100;
+            public static string prompt = "Please type your name followed by the ENTER key.";
+            public static string name;
+            public static bool IsKeyDown = false;
+           
+        }
+    }
+
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass practiceName = new MyClass();
+
+           Console.Write(practiceName.Multi(45, 67));
+            
+            
+
+            //Console.WriteLine(myClass.MyStruct.prompt);
+            myClass.MyStruct.name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + myClass.MyStruct.name);
+            
+            
+           
+
+
+            while (true);
+        }
+    }
+}
